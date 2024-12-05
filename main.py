52898b3edd8c87fe671aa60e52b67e2e39a7dd0d
Douglas <douglasxavier95@gmail.com>
2024-12-05 18:50:34 -0300

    feat: create asteroid field and colission detection

--- main.py
+++ main.py
@@ -2,10 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import ASTEROID_KINDS, ASTEROID_MAX_RADIUS,ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE,SCREEN_HEIGHT,SCREEN_WIDTH
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-def main(): 
+def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     pygame.init()
+
+    dt = 0
+    clock = pygame.time.Clock()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    field = AsteroidField()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,9 +33,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
             
-        screen.fill((0,0,0))
+        for asteroid in asteroids:
+            if asteroid.check_colission(player):
+                print("Game over!")
+                sys.exit()
+        
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        
+        elapsed = clock.tick(60)
+        dt = elapsed/1000 
 
 if __name__ == "__main__":
     main()
